825d500ccc411f4ef31357033e67544bd7460298
mehtameet2802 <mehtameetoff2802@gmail.com>
2023-01-28 19:21:31 +0000

    updates made

--- app.py
+++ app.py
@@ -1,45 +1,11 @@
-import flask
-import numpy as np
-import pandas as pd
-import pickle
-import bz2file as bz2
-from flask import Flask, jsonify, request
 
+# A very simple Flask Hello World app for you to get started with...
 
-def decompress_pickle(file):
-    data = bz2.BZ2File(file, "rb")
-    data = pickle.load(data)
-    return data
-
+from flask import Flask
 
 app = Flask(__name__)
-pickle_in = open("CropRecommendation.pkl", "rb")
-recommender = pickle.load(pickle_in)
-recommender = decompress_pickle("Recommendation.pbz2")
 
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
-@app.route('/', methods=['GET'])
-def index():
-    return 'Hello, World'
-
-
-@app.route('/predict', methods=['POST'])
-def predict_crop():
-    nitrogen = float(request.form['nitrogen'])
-    phosphorus = float(request.form['phosphorus'])
-    potassium = float(request.form['potassium'])
-    sodium = float(request.form['sodium'])
-    iron = float(request.form['iron'])
-    zinc = float(request.form['zinc'])
-    temperature = float(request.form['temperature'])
-    humidity = float(request.form['humidity'])
-    ph = float(request.form['ph'])
-    rainfall = float(request.form['rainfall'])
-    recommended_crop = recommender.predict([[nitrogen, phosphorus, potassium,
-                                             sodium, iron, zinc, temperature, humidity, ph, rainfall]])
-    print(f"prediction = {recommended_crop}")
-    return jsonify(recommended_crop=recommended_crop[0])
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
