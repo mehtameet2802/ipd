07c57ca72d008af32429c0d73d34434dfb0dac35
mehtameet2802 <mehtameetoff2802@gmail.com>
2023-01-29 15:55:10 +0530

    working

--- app1.py
+++ app1.py
@@ -17,16 +17,16 @@ recommender = decompress_pickle("Recommendation.pbz2")
 
 def main():
     st.title('Crop Recommendation')
-    nitrogen = float(st.number_input('nitrogen'))
-    phosphorus = float(st.number_input('phosphorus'))
-    potassium = float(st.number_input('potassium'))
-    sodium = float(st.number_input('sodium'))
-    iron = float(st.number_input('iron'))
-    zinc = float(st.number_input('zinc'))
-    temperature = float(st.number_input('temperature'))
-    humidity = float(st.number_input('humidity'))
-    ph = float(st.number_input('ph'))
-    rainfall = float(st.number_input('rainfall'))
+    nitrogen = st.number_input('nitrogen')
+    phosphorus = st.number_input('phosphorus')
+    potassium = st.number_input('potassium')
+    sodium = st.number_input('sodium')
+    iron = st.number_input('iron')
+    zinc = st.number_input('zinc')
+    temperature = st.number_input('temperature')
+    humidity = st.number_input('humidity')
+    ph = st.number_input('ph')
+    rainfall = st.number_input('rainfall')
 
     recommended_crop = ''
     if st.button('Predict Crop'):
@@ -34,11 +34,6 @@ def main():
                                                  sodium, iron, zinc, temperature, humidity, ph, rainfall]])
     print(f"prediction = {recommended_crop}")
     st.success(recommended_crop[0])
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
 
 
 if __name__ == '__main__':
